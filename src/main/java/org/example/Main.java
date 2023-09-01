975679638e6250e9c00bb11eb45d6777bfe0a3b2
jona-cea <jonycea7@gmail.com>
2023-08-31 22:43:33 -0700

    se agrega el proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //EmpleadoIndefinido empleadoIndefinido = new EmpleadoIndefinido("jona","cea",300000,false );
+    }
+}
